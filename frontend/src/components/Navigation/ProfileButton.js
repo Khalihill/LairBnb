f36e479f61de975829d12c541aa2c28980115280
Khali Hill <khali.hill@gmail.com>
2022-05-31 17:56:34 -0500

    working on heroku

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import "./Navigation.css";
+
+export default function ProfileButton() {
+  return (
+    <div style={{ color: "orange", fontSize: "100px" }}>
+      
+    </div>
+  );
+}
